03211bedd5ce71488796ab7fd1b7cd7d37e1977b
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2020-01-07 11:28:30 -0600

    Max and I worked on initial backend work, figured out the config, db schema, and models.

--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-let config = require(__dirname + "/../config/config.json")[env];
+let config = require(__dirname + "../config/config.json")[env];
 if (process.env.CI) {
-  config = require(__dirname + "/../config/config.json").ci;
+  config = require(__dirname + "../config/config.json").ci;
 }
 const db = {};
 
@@ -16,9 +16,9 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
+    config.blogify_db,
+    config.root,
+    config.root,
     config
   );
 }
